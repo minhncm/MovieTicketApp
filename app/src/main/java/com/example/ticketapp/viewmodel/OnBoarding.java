6459d485251de0eeab165ab38acdc53bc9d3767b
ThanhhDaoo <tranthanhdao132005@gmail.com>
2025-11-17 13:57:12 +0700

    tao OnBoaring

---
+++ app/src/main/java/com/example/ticketapp/viewmodel/OnBoarding.java
@@ -0,0 +1,54 @@
+package com.example.ticketapp.viewmodel.;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Space;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ticketapp.databinding.ActivityOnBoardingBinding;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class OnBoarding extends AppCompatActivity {
+    private ActivityOnBoardingBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        binding = ActivityOnBoardingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        setFirstTimeLaunch();
+    }
+
+    private void setFirstTimeLaunch() {
+        binding.btnGetStarted.setOnClickListener(v -> {
+            // Lưu đã xem onboarding
+            SharedPreferences.Editor editor = getSharedPreferences("AppPrefs", MODE_PRIVATE).edit();
+            editor.putBoolean("isFirstTime", false);
+            editor.apply();
+
+            startActivity(new Intent(this, Splash.class));
+            finish();
+        });
+    }
+}
