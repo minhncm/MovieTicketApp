d63fc4b8a11b9800beeac577bdb6d6663cbc4571
minhncm <ncm071205@gmail.com>
2025-11-18 22:40:48 +0700

    refactor: movie repository

--- app/src/main/java/com/example/ticketapp/data/repository/MovieRepositoryImpl.java
+++ app/src/main/java/com/example/ticketapp/data/repository/MovieRepositoryImpl.java
@@ -6,12 +6,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.ticketapp.data.network.ApiService;
-import com.example.ticketapp.domain.model.Cinema;
-import com.example.ticketapp.domain.model.Res.BookingData;
-import com.example.ticketapp.domain.model.Res.BookingRes;
-import com.example.ticketapp.domain.model.Res.TicketRes;
-import com.example.ticketapp.domain.model.Ticket;
-import com.example.ticketapp.domain.repository.BookingRepository;
+import com.example.ticketapp.domain.model.Movie;
+import com.example.ticketapp.domain.repository.MovieRepository;
 import com.example.ticketapp.utils.Resource;
 
 import java.util.List;
@@ -22,71 +18,38 @@ import javax.inject.Singleton;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
 @Singleton
-public class BookingRepositoryImpl implements BookingRepository {
-    private  final ApiService apiService;
+public class MovieRepositoryImpl implements MovieRepository {
+    private final ApiService apiService;
+
     @Inject
-    public BookingRepositoryImpl(ApiService apiService) {
+    public MovieRepositoryImpl(ApiService apiService) {
         this.apiService = apiService;
     }
 
-    @Override
-    public LiveData<Resource<BookingRes>> bookingTicket(BookingData bookingData) {
-        MutableLiveData<Resource<BookingRes>> data = new MutableLiveData<>();
+    public LiveData<Resource<List<Movie>>> getMovies() {
+        MutableLiveData<Resource<List<Movie>>> data = new MutableLiveData<>();
         data.setValue(Resource.loading());
 
-        apiService.bookTicket(bookingData).enqueue(new Callback<BookingRes>() {
+        apiService.getMovies().enqueue(new Callback<List<Movie>>() {
             @Override
-            public void onResponse(Call<BookingRes> call, Response<BookingRes> response) {
+            public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
                 if (response.isSuccessful()) {
                     data.setValue(Resource.success(response.body()));
                 } else {
-                    Log.d("BookingRepositoryImpl", "Response error: " + response.message());
-
+                    Log.d("MovieRepositoryImpl", "Response error: " + response.message());
                     data.setValue(Resource.error("Lỗi: " + response.message()));
                 }
             }
 
             @Override
-            public void onFailure(Call<BookingRes> call, Throwable t) {
-                Log.d("BookingRepositoryImpl", "Response error: " + t.getMessage());
+            public void onFailure(Call<List<Movie>> call, Throwable t) {
+                Log.d("MovieRepositoryImpl", "Response error: " + t.getMessage());
 
                 data.setValue(Resource.error("Thất bại: " + t.getMessage()));
-            }
-        });
-
-        return data;
-    }
-
-    @Override
-    public LiveData<Resource<List<Ticket>>> fetchTickets(String type,String userId) {
-
-        MutableLiveData<Resource<List<Ticket>>> data = new MutableLiveData<>();
-        data.setValue(Resource.loading());
-
-        apiService.fetchTickets(type,userId).enqueue(new Callback<TicketRes>() {
-            @Override
-            public void onResponse(Call<TicketRes> call, Response<TicketRes> response) {
-                if (response.isSuccessful()) {
-                    TicketRes bookingResponse = response.body();
-                    if (bookingResponse != null)
-                        data.setValue(Resource.success(bookingResponse.getBookingsList()));
-                } else {
-                    data.setValue(Resource.error("Lỗi: " + response.message()));
-                    Log.d("BookingRepositoryImpl", "Response error: " + response.message());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<TicketRes> call, Throwable t) {
-                data.setValue(Resource.error("Thất bại: " + t.getMessage()));
-                Log.d("BookingRepositoryImpl", "Response error: " + t.getMessage());
-
             }
         });
 
         return data;
     }
 }
-
