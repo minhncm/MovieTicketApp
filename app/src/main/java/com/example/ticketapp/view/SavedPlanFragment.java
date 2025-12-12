c22ab6ae83006b35d32e454c81eb3938c4294416
thang <leviethoangthang1234@gmail.com>
2025-12-12 17:49:24 +0700

    delete old savedplan and create new SavedPlanEntity

--- app/src/main/java/com/example/ticketapp/view/SavedPlanFragment.java
+++ app/src/main/java/com/example/ticketapp/view/SavedPlanFragment.java
@@ -4,19 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.ticketapp.databinding.FragmentSavedPlanBinding;
-import com.example.ticketapp.domain.model.SavedPlan;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import dagger.hilt.android.AndroidEntryPoint;
 
 
 public class SavedPlanFragment extends Fragment  {
