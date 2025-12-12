59de92ca0b6977f623feb50ccc9911760c5ea796
thang <leviethoangthang1234@gmail.com>
2025-12-12 17:58:23 +0700

    add CRUD method in SavedPlanDao

--- app/src/main/java/com/example/ticketapp/data/db/SavedPlanDao.java
+++ app/src/main/java/com/example/ticketapp/data/db/SavedPlanDao.java
@@ -1,4 +1,20 @@
 package com.example.ticketapp.data.db;
 
-public class SavedPlanDao {
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Update;
+
+import com.example.ticketapp.domain.model.SavedPlanEntity;
+
+public interface SavedPlanDao  {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(SavedPlanEntity plan);
+
+    @Update
+    void update(SavedPlanEntity plan);
+
+    @Delete
+    void delete(SavedPlanEntity plan);
+
 }
